ccd36b5dcf36584b4184c468f4ffa7ccf0d89dca
pawnesh <pawneshkitio@gmail.com>
2024-11-11 15:22:03 +0530

    Ring, Score and Multiplyer added.

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,7 @@
 import { k } from "../kaplaCTX";
 import { makeSonic } from "../entities/sonic";
 import { makeMotobug } from "../entities/motobug";
+import { makeRing } from "../entities/ring";
 import gameOver from "./gameOver";
 
 export default function game() {
@@ -29,6 +30,20 @@ export default function game() {
       k.add([k.sprite("platforms"),k.pos(0,450),k.scale(4)]),
       k.add([k.sprite("platforms"),k.pos(platformsWidth,450),k.scale(4)]),
     ];
+
+    // Score Variable initializing
+    let score = 0;
+    let scoreMultiplyer =0;
+
+    let scoreText = k.add([
+      k.text("SCORE : 0", { font: "mania", size: 72}),
+      k.pos(20, 20),
+    ]);
+    let multiplyerText = k.add([
+      k.text("1X", { font: "mania", size: 72}),
+      k.pos(950,20),
+    ]);
+
     const sonic = makeSonic(k.vec2(200,745));
     sonic.setControls();
     sonic.setEvents();
@@ -39,11 +54,21 @@ export default function game() {
             k.destroy(enemy);
             sonic.play("jump");
             sonic.jump();
+            scoreMultiplyer +=1 ;
+            score += 10 * scoreMultiplyer;
+            scoreText.text = `SCORE : ${score}`;  
+            multiplyerText.text = `${scoreMultiplyer}X`; 
             return;
         }
         k.play("hurt",{ volume: 0.5 });
         k.go("gameover");
     });
+    sonic.onCollide("ring", (ring)=>{
+        k.play("ring", {volume:0.5});
+        k.destroy(ring);
+        score++;
+        scoreText.text = `SCORE : ${score}`;
+  });
 
     let gameSpeed = 300;
     k.loop(1, ()=>{
@@ -68,6 +93,18 @@ export default function game() {
         k.wait(waitTime, spawnMotoBug);
     };
     spawnMotoBug();
+    const spawnRing= () =>{
+      const ring = makeRing(k.vec2(1950, 745));
+      ring.onUpdate(()=>{
+        ring.move(-gameSpeed, 0);
+      });
+      ring.onExitScreen(()=>{
+
+      });
+      const waitTime = k.rand(0.5, 3);
+      k.wait(waitTime, spawnRing);
+    };
+    spawnRing();
 
     k.add([
         k.rect(1920,300),
@@ -77,6 +114,8 @@ export default function game() {
         k.body({ isStatic: true}),
     ]);
     k.onUpdate( ()=>{
+      if(sonic.isGrounded()) scoreMultiplyer=0;
+      multiplyerText.text = `${scoreMultiplyer}X`; 
               // BG Move
       if(bgPieces[1].pos.x<0 ){
         bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
