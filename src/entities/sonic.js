3211a05141da55f02690dc4ab974632adaf43b4b
pawnesh <pawneshkitio@gmail.com>
2024-11-15 09:47:29 +0530

    Solved infinity jumps problem on keep pressing button

--- src/entities/sonic.js
+++ src/entities/sonic.js
@@ -9,18 +9,27 @@ export function makeSonic(pos){
         k.body({ jumpForce:1700 }),
         {
             ringCollectionUI:null,
+            isJumping: false, // Tracks if Sonic is in the air
+            canJump: true, // Tracks if the jump button has been released
             setControls() {
                 k.onButtonDown("jump", () => {
-                    if(this.isGrounded()){
+                    if(this.isGrounded() && this.canJump){
+                        this.isJumping = true;
+                        this.canJump = false; // Prevents continuous jump until button is released
                         this.play("jump");
                         this.jump();
                         k.play("jump", {volume:0.5});
 
                     }
-                })
+                });
+                k.onButtonRelease("jump", () => {
+                    // Allows jump again only when the button is released
+                    this.canJump = true;
+                });
             },
             setEvents(){
                 this.onGround( ()=> {
+                    this.isJumping = false; // Reset the flag when Sonic lands
                     this.play("run");
                 });
             }
